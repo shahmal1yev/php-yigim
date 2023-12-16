684de59d2bca5b3704a90f55eac133621bd77196
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-12-16 15:07:09 +0400

    Refactor: InitializationJsonResponseHandler class improvements
    - Exception messages for JSON decoding and missing required fields are more descriptive
    - Use specific exception types (LogicException, InvalidArgumentException) for better error handling

--- src/Core/JsonResponseHandler/InitializationJsonResponseHandler.php
+++ src/Core/JsonResponseHandler/InitializationJsonResponseHandler.php
@@ -10,7 +10,28 @@ use Shahmal1yev\EasyPay\Yigim\Traits\JsonResponseHandlerAttribute\UrlJsonRespons
 
 class InitializationJsonResponseHandler implements JsonResponseHandlerContract, InitializationJsonResponseHandlerContract
 {
+    private \stdClass $response;
+    private const array REQUIRED_FIELDS = [
+        'url',
+        'code',
+        'message'
+    ];
+
     use MessageJsonResponseHandlerAttributeTrait,
         CodeJsonResponseHandlerAttributeTrait,
         UrlJsonResponseHandlerAttributeTrait;
+
+    public function __construct(string $json)
+    {
+        $response = json_decode($json);
+
+        if (json_last_error() !== JSON_ERROR_NONE)
+            throw new \InvalidArgumentException("Invalid JSON provided for '{$this::class}': " . json_last_error_msg());
+
+        foreach(self::REQUIRED_FIELDS as $required)
+            if (! isset($response->$required))
+                throw new \LogicException("Required values do not exist in JSON for '{$this::class}'");
+
+        $this->response = $response;
+    }
 }
